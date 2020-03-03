d5330b78970058e2ac0cf1d3cb3a873f0c37fbb9
DrXneaky <33802435+DrXneaky@users.noreply.github.com>
2020-03-03 23:07:49 +0100

    added depends_on

--- web/app/app.py
+++ web/app/app.py
@@ -1,26 +1,28 @@
-# Copyright 2018 Cisco Systems, Inc.
-# All rights reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
 
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://hello_flask:hello_flask@db:5432/hello_flask_dev"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:#########@localhost:5432/postgres"
+db = SQLAlchemy(app)
 
 
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, email):
+        self.email = email
 
 @app.route('/')
 def hello():
-    return "<h1>Hello Container World! I work just fine</h1>"
+    return "<h1>Hello Container World! Postgres works</h1>"
+
+#if __name__ == "__main__":
+   # app.run()
